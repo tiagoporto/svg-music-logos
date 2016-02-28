13f5408d2fb7cfe1b3b9fc712a221b196ef061e8
Tiago Porto <tiagoporto@ymail.com>
2016-02-28 12:30:31 -0300

    Update 2016-02-28T15:30:14.383Z

--- src/scripts/angular/controllers/main.js
+++
@@ -1,8 +0,0 @@
-angular.module('svgBandLogosApp')
-  .controller('MainCtrl', function ($scope, $http) {
-
-    $http.get("data.json")
-      .then(function(response) {
-        $scope.bands = response.data;
-      });
-  });
