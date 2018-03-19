d5af3f16f2dc25171e16d852e5f39e9146ff8ed8
Tiago Porto <tiagoporto@ymail.com>
2018-03-18 22:15:43 -0300

    Separate data logic

--- src/app.vue
+++ src/app.vue
@@ -15,74 +15,11 @@
 </template>
 
 <script>
+import {artists, genres, logos, origins} from './data.js'
 import AppFooter from './components/footer/AppFooter.vue'
 import AppHeader from './components/header/AppHeader.vue'
 import BackTop from './components/back-top/BackTop.vue'
 import Card from './components/card/Card.vue'
-import data from './data.json'
-
-data.sort((a, b) => {
-  const nameA = a.name.toLowerCase()
-  const nameB = b.name.toLowerCase()
-
-  if (nameA < nameB) {
-    return -1
-  }
-  if (nameA > nameB) {
-    return 1
-  }
-  return 0
-})
-
-const allGenres = data
-  .map(elem => {
-    if (process.env.NODE_ENV !== 'production') {
-      elem.genre || console.warn(`${elem.name} is missing the genre.`)
-    }
-
-    return elem.genre
-  })
-  .join()
-  .split(',')
-  .filter(genre => genre)
-
-const genres = allGenres
-  .filter((item, pos) => allGenres.indexOf(item) === pos)
-  .sort()
-
-const allOrigins = data
-  .map(elem => {
-    if (process.env.NODE_ENV !== 'production') {
-      elem.origin || console.warn(`${elem.name} is missing the origin.`)
-    }
-
-    return elem.origin
-  })
-  .join()
-  .split(',')
-  .filter(origin => origin !== undefined)
-
-const origins = allOrigins
-  .filter((item, pos) => allOrigins.indexOf(item) === pos)
-  .sort()
-
-const logos = []
-
-data.forEach((band, index) => {
-  const getLogo = band.logos
-
-  getLogo.forEach(logo => {
-    logos.push({
-      name: band.name,
-      nameTemplate: band.nameTemplate,
-      link: band.link,
-      origin: band.origin,
-      genre: band.genre,
-      css: band.css,
-      logo
-    })
-  })
-})
 
 export default {
   name: 'App',
@@ -94,7 +31,7 @@ export default {
   },
   data () {
     return {
-      artists: data,
+      artists,
       logos,
       genres,
       origins,
