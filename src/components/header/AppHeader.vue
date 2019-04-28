200dcecd6a0607a97e8dec0517729fe93f044ff3
Tiago Porto <tiagoporto@ymail.com>
2019-04-28 19:32:38 -0300

    Add logo and icons
    Close #5
    Close #7

--- src/components/header/AppHeader.vue
+++ src/components/header/AppHeader.vue
@@ -5,30 +5,40 @@
     <div class="header__main">
       <h1 class="header__title">
         <a href="http://tiagoporto.github.io/svg-music-logos" class="header__link-title">
-          <!-- <img id="logo" src="img/logos/logo.svg" alt="{-}" width="250" /> -->
-          SVG Music Logos
+          <img src="img/logos/logo.svg" alt="SVG Music Logos" width="300">
         </a>
         <span class="header__subtitle">{{artists.length}} artists • {{logos.length}} logos</span>
       </h1>
-
 
       <p>A collection of bands' and musicians' logos in SVG.</p>
       <p>All brands are trademarks of their respective bands or musicians.</p>
       <p>The brands and symbols should only be used to represent which artists they refer.</p>
 
-      <p><a href="https://github.com/tiagoporto/svg-music-logos/issues" class="link">Request a new logo or report a problem.</a></p>
+      <p>
+        <a
+          href="https://github.com/tiagoporto/svg-music-logos/issues"
+          class="link"
+        >Request a new logo or report an issue.</a>
+      </p>
 
-      <input type="search" v-model.trim="search.band" placeholder="Search" class="search" aria-label="Search by names" autofocus/>
+      <input
+        type="search"
+        v-model.trim="search.band"
+        placeholder="Search"
+        class="search"
+        aria-label="Search by names"
+        autofocus
+      >
 
       <div class="filter">
         <select v-model="search.origin" class="select" aria-label="Origins">
-            <option value="">All Origins</option>
-            <option v-for="origin in origins" :value="origin">{{origin}}</option>
+          <option value>All Origins</option>
+          <option v-for="origin in origins" :value="origin">{{origin}}</option>
         </select>
 
         <select v-model="search.genre" class="select" aria-label="Genres">
-            <option value="">All Genres</option>
-            <option v-for="genre in genres" :value="genre">{{genre}}</option>
+          <option value>All Genres</option>
+          <option v-for="genre in genres" :value="genre">{{genre}}</option>
         </select>
       </div>
     </div>
@@ -36,27 +46,27 @@
 </template>
 
 <script>
-import './AppHeader.styl'
-import './Jumbotron.styl'
-import _ from 'lodash'
-import GithubCorner from '../github-corner/GithubCorner.vue'
+import "./AppHeader.styl";
+import "./Jumbotron.styl";
+import _ from "lodash";
+import GithubCorner from "../github-corner/GithubCorner.vue";
 
 const setJumbotronHeight = () => {
   if (window.innerWidth > 768) {
     if (window.scrollY > 20) {
-      document.getElementById('jumbotron').style.height = '100%'
+      document.getElementById("jumbotron").style.height = "100%";
     } else {
-      document.getElementById('jumbotron').style.height = '400px'
+      document.getElementById("jumbotron").style.height = "450px";
     }
   } else {
-    document.getElementById('jumbotron').style.height = '100%'
+    document.getElementById("jumbotron").style.height = "100%";
   }
-}
+};
 
-window.addEventListener('scroll', _.debounce(setJumbotronHeight, 20))
+window.addEventListener("scroll", _.debounce(setJumbotronHeight, 20));
 
 export default {
-  name: 'AppHeader',
+  name: "AppHeader",
   components: {
     GithubCorner
   },
@@ -67,5 +77,5 @@ export default {
     search: [Object],
     logos: [Array]
   }
-}
+};
 </script>
