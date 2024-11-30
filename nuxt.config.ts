64ba1cb10cf41f34fb25d2f6502b50ad1337a375
Tiago Porto <tiago@tiagoporto.com>
2024-11-30 11:39:33 -0300

    chore: move code to src folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
+import { defineNuxtConfig } from 'nuxt/config'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  srcDir: 'src/',
 
   vue: {
     compilerOptions: {
