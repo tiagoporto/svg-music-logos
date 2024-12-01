73b38e8b05d8c8f8762c6044569f76080979a5d4
Tiago Porto <tiago@tiagoporto.com>
2024-11-30 21:42:55 -0300

    chore: fix gulp scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,105 @@
-import data from './dump/src/data.js'
+import fs from 'node:fs'
 import { src, dest, series, parallel, watch as gulpWatch } from 'gulp'
 import replace from 'gulp-replace'
 import stylus from 'gulp-stylus'
-import jsonMinify from 'gulp-jsonminify'
 import jsonConcat from 'gulp-concat-json-to-array'
 import changed from 'gulp-changed'
+const getData = () => {
+  const data = JSON.parse(fs.readFileSync('./src/server/db/data.json'))
+
+  data.sort((a, b) => {
+    const nameA = a.name.toLowerCase()
+    const nameB = b.name.toLowerCase()
+
+    if (nameA < nameB) {
+      return -1
+    }
+    if (nameA > nameB) {
+      return 1
+    }
+    return 0
+  })
+
+  const allGenres = data
+    .map((elem) => {
+      if (process.env.NODE_ENV !== 'production') {
+        elem.genres || console.warn(`${elem.name} is missing the genre.`)
+      }
+
+      return elem.genres
+    })
+    .join()
+    .split(',')
+    .filter((genre) => genre)
+
+  const genres = allGenres
+    .filter((item, pos) => allGenres.indexOf(item) === pos)
+    .sort()
+
+  const allOrigins = data
+    .map((elem) => {
+      if (process.env.NODE_ENV !== 'production') {
+        elem.origins || console.warn(`${elem.name} is missing the origin.`)
+      }
+
+      return elem.origins
+    })
+    .join()
+    .split(',')
+    .filter((origin) => origin !== undefined)
+
+  const origins = allOrigins
+    .filter((item, pos) => allOrigins.indexOf(item) === pos)
+    .sort()
+
+  const logos = []
+
+  data.forEach((band, index) => {
+    const getLogo = band.logos
+
+    data[index].folder = getLogo[0].svg.split('.')[0].split('_')[0]
+
+    getLogo.forEach((logo) => {
+      logos.push({
+        ...band,
+        logo,
+      })
+    })
+  })
+
+  return {
+    data,
+    genres,
+    origins,
+    logos,
+  }
+}
 
 const paths = {
-  src: 'dump/src/',
-  public: 'dump/public/',
+  db: 'src/server/db',
+  logos: 'src/public/',
 }
 
 const styles = () => {
-  return src(paths.public + 'logos/**/*.styl')
-    .pipe(changed(paths.public + 'logos', { extension: '.css' }))
+  return src(paths.logos + '**/*.styl')
+    .pipe(changed(paths.logos, { extension: '.css' }))
     .pipe(stylus())
-    .pipe(dest(paths.public + 'logos'))
+    .pipe(dest(paths.logos))
 }
 
 const updateReadme = () => {
+  const { data, genres, origins, logos } = getData()
+
   return src('./README.md')
     .pipe(
       replace(
         /<!-- replace start -->[\W\w]+<!-- replace end -->/,
         `<!-- replace start -->
 
-![Total Artists](https://img.shields.io/badge/artists-${data.artists.length}-blue.svg?style=flat-square)
-![Total Logos](https://img.shields.io/badge/logos-${data.logos.length}-blue.svg?style=flat-square)
-![Total Origins](https://img.shields.io/badge/origins-${data.origins.length}-blue.svg?style=flat-square)
-![Total Genres](https://img.shields.io/badge/genres-${data.genres.length}-blue.svg?style=flat-square)
+![Total Artists](https://img.shields.io/badge/artists-${data.length}-blue.svg?style=flat-square)
+![Total Logos](https://img.shields.io/badge/logos-${logos.length}-blue.svg?style=flat-square)
+![Total Origins](https://img.shields.io/badge/origins-${origins.length}-blue.svg?style=flat-square)
+![Total Genres](https://img.shields.io/badge/genres-${genres.length}-blue.svg?style=flat-square)
 
 <!-- replace end -->`,
       ),
@@ -37,15 +108,14 @@ const updateReadme = () => {
 }
 
 const generateData = () => {
-  return src(paths.public + 'logos/**/*.json')
+  return src(paths.logos + '**/*.json')
     .pipe(
       jsonConcat('data.json', function (data) {
         // do any work on data here
         return new Buffer(JSON.stringify(data))
       }),
     )
-    .pipe(jsonMinify())
-    .pipe(dest(paths.src))
+    .pipe(dest(paths.db))
 }
 
 // ***************************** Tasks ******************************* //
@@ -53,7 +123,8 @@ const generateData = () => {
 export { updateReadme as 'update-readme' }
 export { generateData as 'generate-data' }
 export const watch = () => {
-  gulpWatch(paths.public + 'logos/**/*.styl', styles)
-  gulpWatch(paths.public + 'logos/**/*.json', generateData)
+  gulpWatch(paths.logos + '**/*.styl', styles)
+  gulpWatch(paths.logos + '**/*.json', generateData)
 }
 export const build = series(generateData, parallel(styles, updateReadme))
+export { styles as 'generate-styles' }
