f52a7612cdce103b7e141166a54e3ced750b0494
Tiago Porto <tiago@tiagoporto.com>
2024-12-08 13:18:18 -0300

    Setup commitlint

---
+++ commitlint.config.mjs
@@ -0,0 +1,9 @@
+export default {
+  rules: {
+    'header-case': [2, 'always', 'sentence-case'],
+    'header-max-length': [2, 'always', 100],
+    'header-trim': [2, 'always'],
+    'type-empty': [2, 'always'],
+    'scope-empty': [2, 'always'],
+  },
+}
