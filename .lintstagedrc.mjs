2b7100f63d073f159fc22d813789345f25704425
Tiago Porto <tiago@tiagoporto.com>
2025-04-10 14:27:37 -0300

    Fix lintstaged tsc on vue files

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,7 +1,7 @@
 export default {
   '*': 'prettier --check --ignore-unknown --write',
   '*.{md,markdown,mdx}': 'remark --frail',
-  '*.{ts,tsx, vue}': () => 'vue-tsc --project tsconfig.json',
+  '*.{ts,tsx,vue}': () => 'vue-tsc --project tsconfig.json',
   '*.{js,jsx,ts,tsx,vue}': ['eslint --max-warnings 0'],
   '*.{scss,vue}': ['stylelint --fix'],
 }
