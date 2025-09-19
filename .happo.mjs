1c0ddadf8b9c6ff209dbe83951a1e18de081ff29
Tiago Porto <tiago@tiagoporto.com>
2025-09-19 17:55:00 -0300

    Set happo freezeAnimations

--- .happo.mjs
+++ .happo.mjs
@@ -10,7 +10,7 @@ export default {
     'chrome-large': new RemoteBrowserTarget('chrome', {
       viewport: '1024x768',
       prefersReducedMotion: true,
-      // freezeAnimations: 'last-frame',
+      freezeAnimations: 'last-frame',
       maxHeight: 10000,
     }),
 
