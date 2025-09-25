351cdd8e4a2f1a256076592c8e3554937aeb402f
pranjal.arora <pranjal.arora@officebanao.com>
2025-09-25 22:58:16 +0530

    chore: add Tiago ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,7 @@ import {
 } from '@vue/eslint-config-typescript'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 import vitest from '@vitest/eslint-plugin'
+import tpConfig from '@tiagoporto/eslint-config'
 import playwright from 'eslint-plugin-playwright'
 
 export default withNuxt(
@@ -18,30 +19,52 @@ export default withNuxt(
 
     {
       name: 'app/files-to-ignore',
-      ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+      ignores: [
+        '**/dist/**',
+        '**/dist-ssr/**',
+        '**/coverage/**',
+        '**/*.svg',
+        '**/*.md',
+        '**/*.json',
+        '**/*.yml',
+        '**/*.yaml',
+        'package-lock.json',
+      ],
     },
+
     js.configs.recommended,
     ...pluginVue.configs['flat/recommended'],
     pluginVue.configs['flat/essential'],
     vueTsConfigs.recommended,
     skipFormatting,
+    ...tpConfig.configs.base,
+    {
+      rules: {
+        'import-x/order': 'off',
+        'import-x/no-named-as-default': 'off',
+      },
+    },
     {
       rules: {
         'vue/multi-word-component-names': 'off',
         'vue/no-multiple-template-root': 'off',
         'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
         'no-nested-ternary': 'error',
+        'unicorn/no-null': 'off',
+        'unicorn/filename-case': 'off',
+        'unicorn/no-array-for-each': 'off',
+        'unicorn/prefer-top-level-await': 'off',
+        'no-secrets/no-secrets': 'off',
+        'no-unsafe-optional-chaining': 'warn',
       },
     },
+
     {
       files: ['**/*.test.js'],
-      plugins: {
-        vitest,
-      },
-      rules: {
-        ...vitest.configs.all.rules,
-      },
+      plugins: { vitest },
+      rules: { ...vitest.configs.all.rules },
     },
+
     {
       ...playwright.configs['flat/recommended'],
       files: ['tests/**'],
