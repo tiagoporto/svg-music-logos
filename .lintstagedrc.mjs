1e6a3d59fd291438b07e98a981f922258009f057
Tiago Porto <tiago@tiagoporto.com>
2025-09-26 16:28:02 -0300

    Update lint staged with tests

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,7 +1,14 @@
 export default {
   '*': 'prettier --check --ignore-unknown --write',
-  '*.{md,markdown,mdx}': 'remark --frail',
+  '*.{md,markdown,mdx}': [
+    'remark --frail',
+    // 'eslint --max-warnings 0 --no-warn-ignored'
+  ],
+  // '*.{html,yml,json,jsonc,json5}': 'eslint --max-warnings 0 --no-warn-ignored',
   '*.{ts,tsx,vue}': () => 'vue-tsc --project tsconfig.json',
-  '*.{js,jsx,ts,tsx,vue}': ['eslint --max-warnings 0'],
+  '*.{js,mjs,cjs,jsx,ts,mts,cts,tsx,vue}': [
+    'eslint --max-warnings 0',
+    'vitest related --run --passWithNoTests',
+  ],
   '*.{scss,vue}': ['stylelint --fix'],
 }
